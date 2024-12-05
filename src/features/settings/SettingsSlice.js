42467bba601134dc79a688166fafe9329b71085c
Kaleb Bishop <Kalebwbishop@gmail.com>
2024-12-05 15:48:20 -0500

    Added defaults

--- src/features/settings/SettingsSlice.js
+++ src/features/settings/SettingsSlice.js
@@ -4,7 +4,38 @@ import { getJSONCookie } from "../../utils/cookieUtils";
 const initialState =
   getJSONCookie("settings") ||
   {
-    value: {}
+    value: {
+      filterColumns: {
+        Grade: { active: true },
+        "Parent 1 First": { active: false },
+        "Parent 1 Last": { active: false },
+        "Parent 1 cell": { active: false },
+        "Parent 1 email": { active: false },
+        "Parent 2 First": { active: false },
+        "Parent 2 Last": { active: false },
+        "Parent 2 cell": { active: false },
+        "Parent 2 email": { active: false },
+        "Student Cell": { active: false },
+        "Student Email": { active: false },
+        "Student First": { active: false },
+        "Student Last": { active: false }
+      },
+      sortColumns: {
+        Grade: { active: false },
+        "Parent 1 First": { active: false },
+        "Parent 1 Last": { active: false },
+        "Parent 1 cell": { active: false },
+        "Parent 1 email": { active: false },
+        "Parent 2 First": { active: false },
+        "Parent 2 Last": { active: false },
+        "Parent 2 cell": { active: false },
+        "Parent 2 email": { active: false },
+        "Student Cell": { active: false },
+        "Student Email": { active: false },
+        "Student First": { active: true },
+        "Student Last": { active: true }
+      }
+    }
   };
 
 export const SettingsSlice = createSlice({
@@ -12,6 +43,7 @@ export const SettingsSlice = createSlice({
   initialState: initialState,
   reducers: {
     set: (state, action) => {
+      console.log("SettingsSlice set action", action.payload);
       state.value = action.payload;
     },
   },
