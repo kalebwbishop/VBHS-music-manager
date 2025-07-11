dac3c3f87e30729efb346392c0799f7871bbebe9
kalebwbishop <Kalebwbishop@gmail.com>
2025-07-11 13:40:24 -0400

    Updated auth to not update twice

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,64 +65,57 @@ const login = asyncHandler(async (req, res) => {
   //Destructing the inputs from req.body
   const { email, password } = req.body;
 
-  //created a variable to assign the user
-  let getUser;
+  try {
+    //verifying that the user with the email exist or not
+    const user = await userModel.findOne({ email: email });
+    
+    if (!user) {
+      //if user does not exist responding Authentication Failed
+      return res.status(401).json({
+        message: "Authentication Failed",
+        success: false,
+      });
+    }
 
-  //verifying that the user with the email exist or not
-  userModel
-    .findOne({
-      email: email,
-    })
-    .then((user) => {
-      if (!user) {
-        //if user does not exist responding Authentication Failed
-        return res.status(401).json({
-          message: "Authentication Failed",
-          success: false,
-        });
-      }
-      //assigned the user to getUser variable
-      getUser = user;
-      /*
+    /*
     Then compare the password from the req.body and the hased password on the database 
     using the bcrypt.compare built in function
     */
-      return bcrypt.compare(password, user.password);
-    })
-    .then((response) => {
-      if (!response) {
-        return res.status(401).json({
-          message: "Authentication Failed",
-          success: false,
-        });
-      } else {
-        let jwtToken = jwt.sign(
-          {
-            email: getUser.email,
-            userId: getUser.userId,
-          },
-          //Signign the token with the JWT_SECRET in the .env
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "24h",
-          }
-        );
-        return res.status(200).json({
-          accessToken: jwtToken,
-
-          //I like to send the userId of the user that loggedin in order to fetch his data and dispaly
-          userId: getUser.userId,
-          message: "Authentication Successful",
-          success: true,
-        });
-      }
-    })
-    .catch((err) => {
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+    if (!isPasswordValid) {
       return res.status(401).json({
-        message: err.message,
+        message: "Authentication Failed",
         success: false,
       });
+    }
+
+    // If we reach here, authentication is successful
+    let jwtToken = jwt.sign(
+      {
+        email: user.email,
+        userId: user.userId,
+      },
+      //Signing the token with the JWT_SECRET in the .env
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+    
+    return res.status(200).json({
+      accessToken: jwtToken,
+      //I like to send the userId of the user that loggedin in order to fetch his data and display
+      userId: user.userId,
+      message: "Authentication Successful",
+      success: true,
     });
+  } catch (err) {
+    return res.status(401).json({
+      message: err.message,
+      success: false,
+    });
+  }
 });
 
 const userProfile = asyncHandler(async (req, res, next) => {
