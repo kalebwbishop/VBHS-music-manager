befe9eea6cab14a7b702ee4c2c06235081a0b9f2
kalebwbishop <Kalebwbishop@gmail.com>
2025-07-15 10:37:01 -0400

    Update config.js

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,3 +1,3 @@
 window.env = {
-  REACT_APP_BACKEND_URL: "REACT_APP_BACKEND_URL_PLACEHOLDER",
+  REACT_APP_BACKEND_URL: "https://mern-backend-835051aa00f74df8.wonderfulsea-ba36315d.eastus.azurecontainerapps.io",
 };
